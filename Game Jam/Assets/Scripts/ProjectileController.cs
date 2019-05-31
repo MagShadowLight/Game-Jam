bc2f811620f2d086986e14f10fff5ae15f8ae92a
WILLIAM ROSE <WILLIAM.ROSE.DP@tumwater.k12.wa.us>
2019-05-31 10:16:39 -0700

    added some folder and scripts

---
+++ Game Jam/Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = gameObject.GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.rigidbody.velocity = new Vector2 (2.0f,0.0f);
+    }
+}
